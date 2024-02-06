ff705298825327c5f5ad97faa24d31b7ca3538b8
Maxps <maxroyal.eisenberg@gmail.com>
2024-02-06 01:22:54 -0500

    added css cards to projects

--- mr-portfolio/src/App.js
+++ mr-portfolio/src/App.js
@@ -13,11 +13,11 @@ function App() {
       <header className="header">
         <img src={cute} className="logo" alt="logo"></img>
       </header>
-      <div className="Projects">
+      <div className="section">
         <h1>Welcome to my Portfolio Page</h1>
-        <div>
-          <div>
-            <div>
+        <div className="projects">
+          <div className="project-card">
+            <div className="project-desc">
               <a
                 href="https://jogomez.github.io/MovieDetails/"
                 target="_blank"
@@ -27,12 +27,12 @@ function App() {
               </a>
               , a webpage where you can learn more about your favorite movies.
             </div>
-            <p>
-              <img src={movieGeek} alt="Screenshots of Movie Geek"></img>
-            </p>
+            <div className="project-image">
+              <img src={movieGeek} alt="Screenshots of Movie Geek" height="80px" width="auto"></img>
+            </div>
           </div>
-          <div>
-            <div>
+          <div className="project-card">
+            <div className="project-desc">
               <a
                 href="https://polar-temple-53244.herokuapp.com/"
                 target="_blank"
@@ -43,15 +43,16 @@ function App() {
               , a blog dedicated to sharing pictures of and commenting on cute
               animals.
             </div>
-            <p>
+            <div className="project-image">
               <img
                 src={cowGiraffeShrimp}
                 alt="Screenshot of Rate My Cow, Giraffe or Shrimp"
+                height="80px" width="auto"
               ></img>
-            </p>
+            </div>
           </div>
-          <div>
-            <div>
+          <div className="project-card">
+            <div className="project-desc">
               <a
                 href="https://ancient-ravine-85592.herokuapp.com/"
                 target="_blank"
@@ -63,21 +64,23 @@ function App() {
               app that lets users explore the night sky via stars,
               constellations, moon phase calendars and other celestial events.
             </div>
-            <p>
+            <div className="project-image">
               <img
                 src={main_screen}
                 alt="Screenshot the main page of Star Gazers"
+                height="80px" width="auto"
               ></img>
-            </p>
-            <p>
+            </div>
+            <div className="project-image">
               <img
                 src={moon_phase}
                 alt="Screenshot moon phase page of Star Gazers"
+                height="80px" width="auto"
               ></img>
-            </p>
+            </div>
           </div>
         </div>
-        <p>
+        <div>
           This will be colorful! Nunc pharetra finibus est at efficitur.
           Praesent sed congue diam. Integer gravida dui mauris, ut interdum nunc
           egestas sed. Aenean sed mollis diam. Nunc aliquet risus ac finibus
@@ -85,7 +88,7 @@ function App() {
           aliquet orci porta quam semper imperdiet. Praesent euismod mi justo,
           faucibus scelerisque risus cursus in. Sed rhoncus mollis diam, sit
           amet facilisis lectus blandit at.
-        </p>
+        </div>
         <aside class="aside">
           <h2>Want a lovely e-card?</h2>
           <form>
