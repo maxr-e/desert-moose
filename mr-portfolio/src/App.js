8f1581faf569bef1c82f8c42e06061118e5ac977
Maxps <maxroyal.eisenberg@gmail.com>
2024-02-08 13:24:25 -0500

    added tech images to repo and tech list unsized

--- mr-portfolio/src/App.js
+++ mr-portfolio/src/App.js
@@ -6,6 +6,13 @@ import profilePic from "../src/images/mre_profile.png"
 import movieGeek from "../src/images/moviegeek.png";
 import cowGiraffeShrimp from "../src/images/rate-my-cow-giraffe-shrimp.png";
 import main_screen from "../src/images/main_screen.png";
+import htmlLogo from "../src/images/HTML5_Badge.svg"
+import ccss3Logo from "../src/images/CSS3_logo.svg"
+import jsLogo from "../src/images/js_logo_svg.svg"
+import nodeLogo from "../src/images/node_js_logo.svg"
+import reactLogo from "../src/images/reactLogo.svg"
+import mongoLogo from "../src/images/MongoDB_Fores-Green.svg"
+import arcLogo from "../src/images/ArcGIS_logo_240.png"
 import moon_phase from "../src/images/moon-phase-oakland-screen.png";
 
 function App() {
@@ -18,17 +25,24 @@ function App() {
         <div className="blockus">
         <h1>Welcome to my Portfolio Page</h1>
         </div>
+        <nav>
+          <ul className="navbar">
+            <li><a href="#projects">Projects</a></li>
+            <li><a href="#resume">Resume</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </nav>
         <div className="blockus">
           <p>Greetings, I'm Max. You've found my little corner of the internet!</p>
         </div>
         <break></break>
         <div className="about">
-          <img src={profilePic} alt="Photo of Max"></img>
+          <img src={profilePic} alt="Portrait pf Max"></img>
           <div className="about-body">
             <p>Nunc pharetra finibus est at efficitur. Praesent sed congue diam. Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper imperdiet.</p>
           </div>
         </div>
-        <h2><b>PROJECTS</b></h2>
+        <h2 id="projects"><b>PROJECTS</b></h2>
         <div className="projects">
           <div className="project-card-wrapper">
             <div className="project-card">
@@ -107,10 +121,29 @@ function App() {
             </div>
           </div>
         </div>
-        <div className="resume-wrapper">Resume
-          <div></div>
+        <h2 id="resume"><b>RESUME</b></h2>
+        <div className="resume-wrapper">
+          <div>Technical Skills</div>
+          {/* <img>CSS Merit Badge</img> */}
+            <ul>
+              <li><img src={htmlLogo} className="logo" alt="logo"></img>HTML5 Badge</li>
+              <li><img src={ccss3Logo} className="logo" alt="logo"></img>CSS3 Badge</li>
+              <li><img src={jsLogo} className="logo" alt="logo"></img>VanillaJS Badge</li>
+              <li><img src={nodeLogo} className="logo" alt="logo"></img>NodeJS Badge</li>
+            </ul>
+            <ul>
+              <li><img src={reactLogo} className="logo" alt="logo"></img>React Badge</li>
+              <li><img src={mongoLogo} className="logo" alt="logo"></img>MongoDB Badge</li>
+              <li><img src={arcLogo} className="logo" alt="logo"></img>ArcGIS</li>
+              <li></li>
+            </ul>
+          <div>Customer Service</div>
+          <div>Collaborative Problem-solving</div>
+          <div>Detail-focused</div>
+          <div>Embedded PDF Resume</div>
         </div>
         <div></div>
+        <h2 id="contact"><b>REACH OUT</b></h2>
         <div className="footer">
           <div className="tail">
             This will be colorful! Nunc pharetra finibus est at efficitur.
